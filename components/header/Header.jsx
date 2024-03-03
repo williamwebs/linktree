c6c06ccacbb14d24d1b0198e402cc71f42d3423e
William <anazawilliam1@gmail.com>
2024-03-03 06:41:23 +0100

    env

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,6 +1,9 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
+import LogoutButton from "./LogoutButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 const Header = async () => {
   const session = await getServerSession(authOptions);
@@ -9,16 +12,31 @@ const Header = async () => {
     <header className="bg-white border-b py-4">
       <div className="max-w-5xl mx-auto flex justify-between px-4">
         <div className="flex items-center gap-6">
-          <Link href={"/"}>LinkList</Link>
+          <Link
+            href={"/"}
+            className="text-blue-500 font-bold flex items-center gap-1"
+          >
+            <FontAwesomeIcon icon={faLink} />
+            <span>LinkList</span>
+          </Link>
           <nav className="flex gap-4 text-slate-500 text-sm">
             <Link href={"/about"}>About</Link>
             <Link href={"/pricing"}>Pricing</Link>
             <Link href={"/contact"}>Contact</Link>
           </nav>
         </div>
-        <div className="flex gap-4 text-slate-500 text-sm">
-          <Link href={"/login"}>Sign In</Link>
-          <Link href={"/register"}>Create Account</Link>
+        <div className="flex items-center gap-4 text-slate-500 text-sm">
+          {session ? (
+            <>
+              <Link href={"/account"}>Hello,{session?.user?.name}</Link>
+              <LogoutButton />
+            </>
+          ) : (
+            <>
+              <Link href={"/login"}>Sign In</Link>
+              <Link href={"/register"}>Create Account</Link>
+            </>
+          )}
         </div>
       </div>
     </header>
---
+++ components/header/LogoutButton.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { signOut } from "next-auth/react";
+
+const LogoutButton = () => {
+  return (
+    <button
+      onClick={signOut}
+      className="flex items-center gap-2 border rounded-full py-2 px-4 shadow-md"
+    >
+      <span>Logout</span>
+      <FontAwesomeIcon icon={faRightFromBracket} />
+    </button>
+  );
+};
+
+export default LogoutButton;
