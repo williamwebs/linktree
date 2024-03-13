6bbbaa2e3d2c7ea9b55fd29875a43dc4af34b3fb
William <anazawilliam1@gmail.com>
2024-03-13 04:00:16 +0100

    revert to working commit

--- app/(account)/account/[account]/page.jsx
+++ app/(account)/account/[account]/page.jsx
@@ -1,9 +1,13 @@
 "use client";
 
 import PageSettingsForm from "@/components/form/PageSettingsForm";
+import { useSession } from "next-auth/react";
 import { useParams } from "next/navigation";
 
 const Account = () => {
+  const { data: session } = useSession();
+  console.log(session.user);
+
   const url = useParams();
 
   return <PageSettingsForm />;
